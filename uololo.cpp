b0147ccbd75e56780cf12f64b83e6d344e25aff1
dev-laercio <laercio@maisresultado.com.br>
2013-01-03 04:27:31 -0800

    Create uololo.cpp

---
+++ uololo.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   gateway_email.cpp
+ * Author: laercio
+ *
+ * Created on 12 de Dezembro de 2012, 13:53
+ */
+
+#include <iostream>
+#include <vmime/vmime.hpp>
+
+using namespace std;
+using namespace vmime;
+
+int main() {
+
+    return 0;
+}
+
